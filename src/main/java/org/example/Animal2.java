1397377c694344e9b2716f15bd343e123631940c
HerlinRo <43376892+HerlinRo@users.noreply.github.com>
2025-11-17 23:37:19 +0530

    from remote- Animal2.java

--- src/main/java/org/example/Animal2.java
+++ src/main/java/org/example/Animal2.java
@@ -3,5 +3,6 @@ package org.example;
 public class Animal2 {
     public static void main(String[] args) {
         System.out.println("Animal 2");
+         System.out.println("Animal 2 - from remote");
     }
 }
