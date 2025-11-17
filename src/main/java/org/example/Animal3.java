c5f8b5c8108c6c512ab33ad562970ed1a833c448
HerlinRo <43376892+HerlinRo@users.noreply.github.com>
2025-11-17 23:34:14 +0530

    remote edited
    remote edited

--- src/main/java/org/example/Animal3.java
+++ src/main/java/org/example/Animal3.java
@@ -3,5 +3,6 @@ package org.example;
 public class Animal3 {
     public static void main(String[] args) {
         System.out.println("Animal 3");
+         System.out.println("from remote edited");
     }
 }
